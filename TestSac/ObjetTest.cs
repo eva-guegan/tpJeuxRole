cf08b0fbbfb0cba86c3f3fe6e2450f7afcc82683
Eva Guegan <eva.guegan@hotmail.fr>
2020-01-02 15:03:24 +0100

    mise en plce de tests sur sac et objet

---
+++ TestSac/ObjetTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestSac
+{
+    [TestClass]
+    class ObjetTest
+    {
+        private string nom;
+
+        [TestMethod]
+        public void Objet(string nom)
+        {
+            this.nom = nom;
+        }
+
+        [TestMethod]
+        public void AfficherNomObjetTest()
+        {
+            Console.WriteLine("- " + this.nom);
+        }
+    }
+}
---
+++ TestSac/SacTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TestSac
+{
+    [TestClass]
+    class SacTest
+    {
+        Stack<ObjetTest> contenuSac = new Stack<ObjetTest>(5);
+
+        [TestMethod]
+        public void AjouterTest(ObjetTest objet)
+        {
+            contenuSac.Push(objet);
+        }
+
+        [TestMethod]
+        public void AfficherTest()
+        {
+            foreach (ObjetTest nom in contenuSac)
+            {
+                nom.AfficherNomObjetTest();
+            }
+        }
+
+        [TestMethod]
+        public void SupprimerTest()
+        {
+            contenuSac.Pop();
+        }
+    }
+}
